5d8f2791c71e5cf6a6fce6a94c203c0fbb31c1c7
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-03-28 14:46:00 +0200

    adds changes for local storage and lifecycle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,27 @@ import Section from './Section';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
+import contactsList from 'utils/contactsList';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
     name: '',
     number: '',
+  };
+  componentDidUpdate = prevState => {
+    const prevStateStr = JSON.stringify(prevState.contacts);
+    const curStateStr = JSON.stringify(this.state.contacts);
+    if (prevStateStr !== curStateStr) {
+      localStorage.setItem('contacts', curStateStr);
+    }
+  };
+
+  componentDidMount = () => {
+    const contactsSaved = localStorage.getItem('contacts');
+    const contacts = JSON.parse(contactsSaved) || contactsList;
+    this.setState({ contacts });
   };
 
   formSubmitHandler = newContact => {
